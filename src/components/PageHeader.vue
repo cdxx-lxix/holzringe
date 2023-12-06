41892a909fe2b3ce91208716c8f9d1ca210ff72d
CDXX-LXIX <taocross666@gmail.com>
2023-12-06 21:15:02 +0100

    Header, footer, icons created (unstyled)

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,9 +1,58 @@
 <template>
     <header>
-
+        <nav
+            class="bg-white dark:bg-gray-900 w-full z-20 border-b border-gray-200 dark:border-gray-600">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img :src="headerContent.logolink" class="h-8" alt="Holzringe Logo">
+                </div>
+                <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <label class="relative inline-flex items-center me-5 cursor-pointer">
+                        <input type="checkbox" value="" class="sr-only peer" checked>
+                        <div
+                            class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500">
+                        </div>
+                    </label>
+                    <button data-collapse-toggle="navbar-sticky" type="button"
+                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-sticky" aria-expanded="false">
+                        <span class="sr-only">{{ headerContent.srmenutip }}</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <ul
+                        class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <RouterLink to="/"
+                                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                aria-current="page">{{ headerContent.menuhome }}</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/gallery"
+                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ headerContent.menugallery }}</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/benefits"
+                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ headerContent.menubenefits }}</RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="/contacts"
+                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ headerContent.menucontacts }}</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 </template>
 
 <script setup>
+import { RouterLink } from 'vue-router'
+import { headerContent } from '@/content'
 
 </script>
---
+++ src/content.js
@@ -0,0 +1,26 @@
+export const headerContent = {
+    logolink: "#",
+    srmenutip: "Открыть меню",
+    menuhome: "Главная",
+    menugallery: "Галлерея",
+    menubenefits: "Преимущества",
+    menucontacts: "Контакты"
+}
+
+export const footerContent = {
+    holz: "https://holzringe.ru/",
+    creator: "https://mladich.com/",
+    year: new Date().getFullYear(),
+    tel: "#",
+    teltip: "Телефон",
+    telsr: "Позвонить на мобильный",
+    wa: "#",
+    watip: "Whatsapp",
+    wasr: "Связаться в whatsapp",
+    vb: "#",
+    vbtip: "Viber",
+    vbsr: "Связаться в viber",
+    tg: "#",
+    tgtip: "Telegram",
+    tgsr: "Связаться в telegram",
+}
