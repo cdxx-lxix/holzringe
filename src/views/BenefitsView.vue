0986b8d5e28ed4ddd260036c2cd57acc8b1b0db8
CDXX-LXIX <taocross666@gmail.com>
2023-12-11 23:52:16 +0100

    BenefitsView progression

--- src/views/BenefitsView.vue
+++ src/views/BenefitsView.vue
@@ -1,5 +1,54 @@
-<template> 
-    <main>
-        Benefits
+<template>
+    <main class="font-text p-3 mx-auto antialiased w-full max-w-screen-2xl block">
+        <div class="mx-auto my-2 max-h-full rounded-lg">
+            <div
+                class="grid rounded-t-lg shadow-sm md:mb-12 md:grid-cols-2 gap-2">
+                <figure
+                    class="card-image flex flex-col items-center justify-center p-8 text-center border border-gray-800 rounded-lg">
+                    <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
+                        <p class="my-4">If you care for your time, I hands down would go with this."</p>
+                    </blockquote>
+                </figure>
+                <figure
+                    class="card-image flex flex-col items-center justify-center p-8 text-center border border-gray-800 rounded-lg">
+                    <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project
+                        </h3>
+                        <p class="my-4">Designing with Figma components that can be easily translated to the utility classes
+                            of Tailwind CSS is a huge timesaver!"</p>
+                    </blockquote>
+                </figure>
+                <figure
+                    class="card-image flex flex-col items-center justify-center p-8 text-center border border-gray-800 rounded-lg">
+                    <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow</h3>
+                        <p class="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level
+                            up your next application."</p>
+                    </blockquote>
+                </figure>
+                <figure
+                    class="card-image flex flex-col items-center justify-center p-8 text-center border border-gray-800 rounded-lg">
+                    <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>
+                        <p class="my-4">You have many examples that can be used to create a fast prototype for your team."
+                        </p>
+                    </blockquote>
+                </figure>
+            </div>
+            <hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700 mx-3">
+            <div class="w-full max-h-full min-h-max grid grid-cols-3 mt-2 mx-auto">
+                <div class="mx-auto">Text</div>
+                <div class="mx-auto">Text</div>
+                <div class="mx-auto">Text</div>
+            </div>
+        </div>
     </main>
 </template>
+
+<style scoped>
+.card-image {
+    background-image: url("https://www.dolomiti.it/storage/services/24215/conversions/escursione_alta_montagna_shutterstock-desktop.jpg");
+    background-size: cover;
+}
+</style>
