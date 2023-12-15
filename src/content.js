9db2f33f7482f1371cb4ab7615ef6173bb6bdcfc
CDXX-LXIX <taocross666@gmail.com>
2023-12-15 19:51:56 +0100

    HomeView rotating cards introduced

--- src/content.js
+++ src/content.js
@@ -1,76 +1,449 @@
-import logo from '@/assets/images/holzringe_transparent_512_cutbw.png';
+import logo from '@/assets/images/holzringe_transparent_512_cutbw.png'
+import acacia from "@/assets/woodtypes/acacia.webp";
+import alder from "@/assets/woodtypes/alder.webp";
+import ash from "@/assets/woodtypes/ash.webp";
+import bamboo from "@/assets/woodtypes/bamboo.webp";
+import beech from "@/assets/woodtypes/beech.webp";
+import birch from "@/assets/woodtypes/birch.webp";
+import bocote from "@/assets/woodtypes/bocote.webp";
+import bubinga from "@/assets/woodtypes/bubinga.webp";
+import cedar from "@/assets/woodtypes/cedar.webp";
+import cherry from "@/assets/woodtypes/cherry.webp";
+import cocobolo from "@/assets/woodtypes/cocobolo.webp";
+import cypress from "@/assets/woodtypes/cypress.webp";
+import ebony from "@/assets/woodtypes/ebony.webp";
+import fir from "@/assets/woodtypes/fir.webp";
+import goncaloAlves from "@/assets/woodtypes/goncaloalves.webp";
+import granadillo from "@/assets/woodtypes/granadillo.webp";
+import hemlock from "@/assets/woodtypes/hemlock.webp";
+import hickory from "@/assets/woodtypes/hickory.webp";
+import kauri from "@/assets/woodtypes/kauri.webp";
+import larch from "@/assets/woodtypes/larch.webp";
+import lignumVitae from "@/assets/woodtypes/lignumvitae.webp";
+import mahogany from "@/assets/woodtypes/mahogany.webp";
+import mango from "@/assets/woodtypes/mango.webp";
+import maple from "@/assets/woodtypes/maple.webp";
+import merbau from "@/assets/woodtypes/merbau.webp";
+import oak from "@/assets/woodtypes/oak.webp";
+import padauk from "@/assets/woodtypes/padauk.webp";
+import pine from "@/assets/woodtypes/pine.webp";
+import poplar from "@/assets/woodtypes/poplar.webp";
+import purpleheart from "@/assets/woodtypes/purpleheart.webp";
+import redwood from "@/assets/woodtypes/redwood.webp";
+import rosewood from "@/assets/woodtypes/rosewood.webp";
+import sapele from "@/assets/woodtypes/sapele.webp";
+import satinwood from "@/assets/woodtypes/satinwood.webp";
+import spruce from "@/assets/woodtypes/spruce.webp";
+import teak from "@/assets/woodtypes/teak.webp";
+import tulipwood from "@/assets/woodtypes/tulipwood.webp";
+import walnut from "@/assets/woodtypes/walnut.webp";
+import wenge from "@/assets/woodtypes/wenge.webp";
+import zebrawood from "@/assets/woodtypes/zebrawood.webp";
 
-export const projectName = "HOLZRINGE";
+export const projectName = 'HOLZRINGE'
 
 export const headerContent = {
-    logolink: logo,
-    srmenutip: "Открыть меню",
-    menu: {
-        menuhome: {name: "Главная", link: "/"},
-        menugallery: {name: "Галлерея", link: "/gallery"},
-        menubenefits: {name: "Преимущества", link: "/benefits"},
-        menucontacts: {name: "Контакты", link: "/contacts"}
-    },
+  logolink: logo,
+  srmenutip: 'Открыть меню',
+  menu: {
+    menuhome: { name: 'Главная', link: '/' },
+    menugallery: { name: 'Галлерея', link: '/gallery' },
+    menubenefits: { name: 'Преимущества', link: '/benefits' },
+    menucontacts: { name: 'Контакты', link: '/contacts' }
+  }
 }
 
 export const footerContent = {
-    holz: "https://holzringe.ru/",
-    creator: "https://mladich.com/",
-    year: new Date().getFullYear(),
+  holz: 'https://holzringe.ru/',
+  creator: 'https://mladich.com/',
+  year: new Date().getFullYear()
 }
 
 export const contactOptions = {
-    phone: "#",
-    whatsapp: "#",
-    viber: "#",
-    telegram: "#",
+  phone: '#',
+  whatsapp: '#',
+  viber: '#',
+  telegram: '#'
 }
 
 export const contactsPage = {
-    conditionsTitle: "Условия заказа",
-    conditionsCalls: "Ежедневно с 9:00 до 21:00 по МСК",
-    conditions: 'Выезд по Москве для консультации и предварительного просмотра объекта бесплатный. По Московской области 500 рублей. Услуги и сроки реставрации обговариваются после просмотра объекта. Стоимость работ от 1400 руб за м². Для выполнения циклёвочных работ вы оплачиваете материалы, после чего их привозят на объект. Минимальный заказ от 15 метров.',
-    aboutTitle: "О нас",
-    about: "Более 4 лет мы профессионально занимаемся реставрацией паркета и элементов декора из дерева в Москве и Московской области. Во время работы используется только собственное оборудование Lagler: Hummel, Trio, Flip. И профессиональная химия: Ciranova, Bona, Konig, Berger.",
-    form: {
-        mailLabel: "Ваш email",
-        mailPlaceholder: "vyatich@yandex.ru",
-        nameLabel: "Ваше имя",
-        namePlaceholder: "Добрыня",
-        locationLabel: "Локация",
-        locations: {
-            one: "Москва",
-            two: "Московская область",
-            three: "Другой регион"
-        },
-        messageLabel: "Сообщение",
-        messagePlaceholder: "Опишите детали Вашего заказа",
-        fileLabel: "Прикрепить файл",
-        filePlaceholder: "Дополнительные детали по заказу, например фото.",
-        sendButton: "Отправить"
-    }
+  conditionsTitle: 'Условия заказа',
+  conditionsCalls: 'Ежедневно с 9:00 до 21:00 по МСК',
+  conditions:
+    'Выезд по Москве для консультации и предварительного просмотра объекта бесплатный. По Московской области 500 рублей. Услуги и сроки реставрации обговариваются после просмотра объекта. Стоимость работ от 1400 руб за м². Для выполнения циклёвочных работ вы оплачиваете материалы, после чего их привозят на объект. Минимальный заказ от 15 метров.',
+  aboutTitle: 'О нас',
+  about:
+    'Более 4 лет мы профессионально занимаемся реставрацией паркета и элементов декора из дерева в Москве и Московской области. Во время работы используется только собственное оборудование Lagler: Hummel, Trio, Flip. И профессиональная химия: Ciranova, Bona, Konig, Berger.',
+  form: {
+    mailLabel: 'Ваш email',
+    mailPlaceholder: 'vyatich@yandex.ru',
+    nameLabel: 'Ваше имя',
+    namePlaceholder: 'Добрыня',
+    locationLabel: 'Локация',
+    locations: {
+      one: 'Москва',
+      two: 'Московская область',
+      three: 'Другой регион'
+    },
+    messageLabel: 'Сообщение',
+    messagePlaceholder: 'Опишите детали Вашего заказа',
+    fileLabel: 'Прикрепить файл',
+    filePlaceholder: 'Дополнительные детали по заказу, например фото.',
+    sendButton: 'Отправить'
+  }
 }
 
 export const benefitsPage = {
-    subheaderP: "Что Вы получите выбрав нас:"
+  subheaderP: 'Что Вы получите выбрав нас:'
 }
 
 export const galleryPage = {
-    images: [
-        { after: "", before: 'https://1.bp.blogspot.com/-Obtd1tUq1Oo/U6woYy5Uq0I/AAAAAAAAkMo/PxDgOOHyTcQ/s1600/ozadje_psj_6_3.jpg', alt: 'cave', reviewer: 'Andrey1', review: 'Wsjo zbs', comment: "Жопу порвал за этот паркет ёбаный", },
-        { after: "", before: 'https://offloadmedia.feverup.com/parissecret.com/wp-content/uploads/2020/04/23133412/grotte-son-doong-vietnam.jpg', alt: 'cave', reviewer: 'Andrey2', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://bonpic.com/download_img.php?dimg=7157&raz=1440x900', alt: 'cave', reviewer: 'Andrey3', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://www.nastol.com.ua/pic/201405/1440x900/nastol.com.ua-98779.jpg', alt: 'cave', reviewer: 'Andrey4', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://www.desktopbackground.org/download/1440x900/2012/04/15/374861_cave-nature-landscape-wallpapers-hd_1600x1071_h.jpg', alt: 'cave', reviewer: 'Andrey5', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://www.zastavki.com/pictures/1440x900/2017World_Marble_Cathedral_cave_on_the_shores_of_Lake_General_Carrera__Patagonia_Chile_112735_14.jpg', alt: 'cave', reviewer: 'Andrey6', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://namonitore.ru/uploads/catalog/nature/meksika_underground_lake_in_a_cavern_1440.webp', alt: 'cave', reviewer: 'Andrey7', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://cdn.fishki.net/upload/post/2019/04/21/2954536/21c1416c453b6facac7d26f4dcad3e5c.jpg', alt: 'cave', reviewer: 'Andrey8', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://www.desktopbackground.org/download/1440x900/2011/01/09/139423_glacier-cave-wallpapers-wallpaper_1920x1080_h.jpg', alt: 'cave', reviewer: 'Andrey9', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://www.desktopbackground.org/download/1440x900/2012/04/15/374779_adventure-journal-wallpapers-wednesday-cave-mexico_1680x1050_h.jpg', alt: 'cave', reviewer: 'Andrey10', review: 'Wsjo zbs', comment: "", },
-        { after: "", before: 'https://10wallpaper.com/wallpaper/1440x900/1401/cave_in_valley-Nature_HD_Wallpaper_1440x900.jpg', alt: 'cave', reviewer: 'Andrey11', review: 'Wsjo zbs', comment: "", }
-    ],
-    reviewTitle: "Отзыв клиента",
-    commentTitle: "Наш комментарий",
-    beforeTitle: "было",
-    afterTitle: "стало"
+  images: [
+    {
+      after: '',
+      before:
+        'https://1.bp.blogspot.com/-Obtd1tUq1Oo/U6woYy5Uq0I/AAAAAAAAkMo/PxDgOOHyTcQ/s1600/ozadje_psj_6_3.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey1',
+      review: 'Wsjo zbs',
+      comment: 'Жопу порвал за этот паркет ёбаный'
+    },
+    {
+      after: '',
+      before:
+        'https://offloadmedia.feverup.com/parissecret.com/wp-content/uploads/2020/04/23133412/grotte-son-doong-vietnam.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey2',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before: 'https://bonpic.com/download_img.php?dimg=7157&raz=1440x900',
+      alt: 'cave',
+      reviewer: 'Andrey3',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before: 'https://www.nastol.com.ua/pic/201405/1440x900/nastol.com.ua-98779.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey4',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://www.desktopbackground.org/download/1440x900/2012/04/15/374861_cave-nature-landscape-wallpapers-hd_1600x1071_h.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey5',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://www.zastavki.com/pictures/1440x900/2017World_Marble_Cathedral_cave_on_the_shores_of_Lake_General_Carrera__Patagonia_Chile_112735_14.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey6',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://namonitore.ru/uploads/catalog/nature/meksika_underground_lake_in_a_cavern_1440.webp',
+      alt: 'cave',
+      reviewer: 'Andrey7',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://cdn.fishki.net/upload/post/2019/04/21/2954536/21c1416c453b6facac7d26f4dcad3e5c.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey8',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://www.desktopbackground.org/download/1440x900/2011/01/09/139423_glacier-cave-wallpapers-wallpaper_1920x1080_h.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey9',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://www.desktopbackground.org/download/1440x900/2012/04/15/374779_adventure-journal-wallpapers-wednesday-cave-mexico_1680x1050_h.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey10',
+      review: 'Wsjo zbs',
+      comment: ''
+    },
+    {
+      after: '',
+      before:
+        'https://10wallpaper.com/wallpaper/1440x900/1401/cave_in_valley-Nature_HD_Wallpaper_1440x900.jpg',
+      alt: 'cave',
+      reviewer: 'Andrey11',
+      review: 'Wsjo zbs',
+      comment: ''
+    }
+  ],
+  reviewTitle: 'Отзыв клиента',
+  commentTitle: 'Наш комментарий',
+  beforeTitle: 'было',
+  afterTitle: 'стало'
+}
+
+export const homePage = {
+  woodtypes: {
+    1: {
+      name: 'acacia',
+      link: acacia,
+      classes: '',
+      color: ''
+    },
+    2: {
+      name: 'alder',
+      link: alder,
+      classes: '',
+      color: ''
+    },
+    3: {
+      name: 'ash',
+      link: ash,
+      classes: '',
+      color: ''
+    },
+    4: {
+      name: 'bamboo',
+      link: bamboo,
+      classes: '',
+      color: ''
+    },
+    5: {
+      name: 'beech',
+      link: beech,
+      classes: '',
+      color: ''
+    },
+    6: {
+      name: 'birch',
+      link: birch,
+      classes: '',
+      color: ''
+    },
+    7: {
+      name: 'bocote',
+      link: bocote,
+      classes: '',
+      color: ''
+    },
+    8: {
+      name: 'bubinga',
+      link: bubinga,
+      classes: '',
+      color: ''
+    },
+    9: {
+      name: 'cedar',
+      link: cedar,
+      classes: '',
+      color: ''
+    },
+    10: {
+      name: 'cherry',
+      link: cherry,
+      classes: '',
+      color: ''
+    },
+    11: {
+      name: 'cocobolo',
+      link: cocobolo,
+      classes: '',
+      color: ''
+    },
+    12: {
+      name: 'cypress',
+      link: cypress,
+      classes: '',
+      color: ''
+    },
+    13: {
+      name: 'ebony',
+      link: ebony,
+      classes: '',
+      color: ''
+    },
+    14: {
+      name: 'fir',
+      link: fir,
+      classes: '',
+      color: ''
+    },
+    15: {
+      name: 'goncalo alves',
+      link: goncaloAlves,
+      classes: '',
+      color: ''
+    },
+    16: {
+      name: 'granadillo',
+      link: granadillo,
+      classes: '',
+      color: ''
+    },
+    17: {
+      name: 'hemlock',
+      link: hemlock,
+      classes: '',
+      color: ''
+    },
+    18: {
+      name: 'hickory',
+      link: hickory,
+      classes: '',
+      color: ''
+    },
+    19: {
+      name: 'kauri',
+      link: kauri,
+      classes: '',
+      color: ''
+    },
+    20: {
+      name: 'larch',
+      link: larch,
+      classes: '',
+      color: ''
+    },
+    21: {
+      name: 'lignum vitae',
+      link: lignumVitae,
+      classes: '',
+      color: ''
+    },
+    22: {
+      name: 'mahogany',
+      link: mahogany,
+      classes: '',
+      color: ''
+    },
+    23: {
+      name: 'mango',
+      link: mango,
+      classes: '',
+      color: ''
+    },
+    24: {
+      name: 'maple',
+      link: maple,
+      classes: '',
+      color: ''
+    },
+    25: {
+      name: 'merbau',
+      link: merbau,
+      classes: '',
+      color: ''
+    },
+    26: {
+      name: 'oak',
+      link: oak,
+      classes: '',
+      color: ''
+    },
+    27: {
+      name: 'padauk',
+      link: padauk,
+      classes: '',
+      color: ''
+    },
+    28: {
+      name: 'pine',
+      link: pine,
+      classes: '',
+      color: ''
+    },
+    29: {
+      name: 'poplar',
+      link: poplar,
+      classes: '',
+      color: ''
+    },
+    30: {
+      name: 'purpleheart',
+      link: purpleheart,
+      classes: '',
+      color: ''
+    },
+    31: {
+      name: 'redwood',
+      link: redwood,
+      classes: '',
+      color: ''
+    },
+    32: {
+      name: 'rosewood',
+      link: rosewood,
+      classes: '',
+      color: ''
+    },
+    33: {
+      name: 'sapele',
+      link: sapele,
+      classes: '',
+      color: ''
+    },
+    34: {
+      name: 'satinwood',
+      link: satinwood,
+      classes: '',
+      color: ''
+    },
+    35: {
+      name: 'spruce',
+      link: spruce,
+      classes: '',
+      color: ''
+    },
+    36: {
+      name: 'teak',
+      link: teak,
+      classes: '',
+      color: ''
+    },
+    37: {
+      name: 'tulipwood',
+      link: tulipwood,
+      classes: '',
+      color: ''
+    },
+    38: {
+      name: 'walnut',
+      link: walnut,
+      classes: '',
+      color: ''
+    },
+    39: {
+      name: 'wenge',
+      link: wenge,
+      classes: '',
+      color: ''
+    },
+    40: {
+      name: 'zebrawood',
+      link: zebrawood,
+      classes: '',
+      color: ''
+    }
+  }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
     <main class="p-3 mx-auto antialiased w-full max-w-screen-2xl grid grid-cols-12 gap-3 justify-center items-center">
-        <div class="holz-cross-tile dark:holz-cross-tile-light holz-card w-16 h-16 md:w-22 md:h-22 lg:w-28 lg:h-28 flex flex-col items-center justify-center hover:bg-amber-500"
-            v-for="i in 40" :key="i">
-            {{ i }}
+        <div class="flip-card" v-for="wood in homePage.woodtypes" :key="wood">
+           <div class="flip-card-inner w-full h-28">
+               <div class="flip-card-front holz-card w-full h-full flex flex-col items-center justify-center fill-primary-1000 dark:fill-primary-0">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" xml:space="preserve"><path d="M84.707 68.752 65.951 49.998l18.75-18.752a1.989 1.989 0 0 0 0-2.813L71.566 15.295a1.99 1.99 0 0 0-2.814 0L49.999 34.047l-18.75-18.752c-.746-.747-2.067-.747-2.814 0L15.297 28.431a1.992 1.992 0 0 0 0 2.814L34.05 49.998 15.294 68.753a1.993 1.993 0 0 0 0 2.814L28.43 84.704a1.988 1.988 0 0 0 2.814 0l18.755-18.755 18.756 18.754c.389.388.896.583 1.407.583s1.019-.195 1.408-.583l13.138-13.137a1.99 1.99 0 0 0-.001-2.814z"/></svg>
+               </div>
+               <div class="flip-card-back holz-card w-full h-full flex flex-col items-center justify-center" :style='{ backgroundImage: "url(" + wood.link + ")", }'>
+                  <h6 class="font-semibold bg-primary-0/30 text-primary-1000 dark:bg-primary-1000/30 dark:text-primary-0 p-1 rounded-lg font-text text-sm uppercase">{{ wood.name }}</h6>
+               </div>
+           </div>
         </div>
         <div
             class="w-full h-full holz-card col-span-8 row-span-4 col-start-3 row-start-2 flex flex-col items-center justify-center">
@@ -13,10 +19,8 @@
 </template>
 
 <script setup>
-// TODO: change tiles on hover to display type of wood its name, hex and color background in it.
-// change cross color according to theme and remove it on hover
+import { homePage } from '@/content';
+
 // make tiles randomly show the other side of tiles so the user may get the idea
-const woodTypes = {
-    birch: { classes: "hover:bg-amber-500", name: "БЕРЕЗА", color: "#EBE2D3" }
-}
+
 </script>
