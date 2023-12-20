5ff296cda37c14e58358a219a0ac97a89878c11e
CDXX-LXIX <taocross666@gmail.com>
2023-12-20 17:56:06 +0100

    Vercel speedinsights

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@
       <component :is="Component" :key="route.path" />
     </transition>
   </router-view>
-
+  <SpeedInsights/>
   <PageFooter />
 </template>
 
 <script setup>
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import PageHeader from './components/PageHeader.vue';
 import PageFooter from './components/PageFooter.vue';
 import { RouterView } from 'vue-router'
