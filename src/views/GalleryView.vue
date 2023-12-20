7674fd8fc0d3caef84a9174aa51408901cb381d3
CDXX-LXIX <taocross666@gmail.com>
2023-12-20 14:42:16 +0100

    GalleryView mobile adaptation and refactor

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,12 +1,10 @@
 <template>
     <main
         class="p-3 mx-auto antialiased w-full max-w-screen-2xl grid grid-cols-1 md:grid-cols-6 lg:grid-cols-12 gap-3">
-        <div class="holz-card flex flex-col items-center col-span-12 lg:col-span-9 xl:col-span-10 h-full">
+        <div class="holz-card flex flex-col items-center col-span-12 lg:col-span-12 xl:col-span-9 h-full">
             <div class="holz-gallery-controls relative flex flex-row justify-between items-center py-1 px-3 top-0 w-full">
                 <button class="rounded-full h-8 w-8 p-1" @click="imageSliderControls('back')">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z" />
-                    </svg>
+                    <LeftArrow />
                 </button>
                 <div class="text-center">
                     <h6 class="font-menu text-md">{{ `${currentImageIndex + 1} из
@@ -14,9 +12,7 @@
                     <p class="font-text text-sm">Нажмите на фото для просмотра как {{ !isBefore ? galleryPage.beforeTitle : galleryPage.afterTitle }}</p>
                 </div>
                 <button class="rounded-full h-8 w-8 p-1" @click="imageSliderControls('forward')">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z" />
-                    </svg>
+                    <RightArrow />
                 </button>
             </div>
             <img class="object-cover h-full w-full aspect-square md:aspect-video"
@@ -24,13 +20,13 @@
                 :alt="galleryPage.images[currentImageIndex].alt"
                 @click="beforeAfter">
         </div>
-        <div class="flex flex-col items-center col-span-12 lg:col-span-3 xl:col-span-2">
-            <div class="holz-card w-full h-full lg:h-1/2 mb-3 p-3">
+        <div class="flex flex-col lg:flex-row xl:flex-col items-center col-span-12 xl:col-span-3">
+            <div class="holz-card w-full h-full xl:h-1/2 mb-3 lg:mb-0 xl:mb-3 p-3 lg:mr-3 xl:mr-0">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ galleryPage.reviewTitle }}</h5>
                 <p class="mb-2 font-semibold underline">{{ galleryPage.images[currentImageIndex].reviewer }}</p>
                 <p class="font-normal">{{ galleryPage.images[currentImageIndex].review }}</p>
             </div>
-            <div class="holz-card w-full h-full lg:h-1/2 p-3">
+            <div class="holz-card w-full h-full xl:h-1/2 p-3">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ galleryPage.commentTitle }}</h5>
                 <p class="font-normal">{{ galleryPage.images[currentImageIndex].comment }}</p>
             </div>
@@ -41,6 +37,9 @@
 <script setup>
 import { ref } from 'vue';
 import { galleryPage } from '@/content';
+import RightArrow from '../components/icons/RightArrow.vue'
+import LeftArrow from '../components/icons/LeftArrow.vue'
+
 const currentImageIndex = ref(0)
 const isBefore = ref(true)
 const imageSliderControls = (direction) => {
